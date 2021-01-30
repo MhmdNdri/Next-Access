59346aca13fc5ef10ebce300ec3d6887481e2aa9
mhdmndri <m.n.mohammad.naderi@gmail.com>
2021-01-30 18:44:37 +0330

    Fix By Farhad

--- next.config.js
+++ next.config.js
@@ -2,20 +2,17 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
-    pwa: {
-        dest: 'public',
-        disable: false,
-        register: true,
-        scope: '/',
-        sw: 'sw.js',
-        subdomainPrefix: '/pwa-sample',
-        runtimeCaching,
-    },
     basePath: process.env.ASSETS_PREFIX,
     assetPrefix: process.env.ASSETS_PREFIX,
     images: {
         loader: 'imgix',
         path: process.env.ASSETS_PREFIX,
+    },
+    pwa: {
+        dest: 'public',
+        runtimeCaching,
+        subdomainPrefix: '/pwa-sample',
+
     },
 
 })
