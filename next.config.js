28cf77cd55b4c2af812235be2363acb0f37dff6d
Mohammad Roshangara <mroshangara@gmail.com>
2021-01-30 13:54:09 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const withPWA = require('next-pwa')
 
 module.exports = withPWA({
     pwa: {
+        disable: process.env.NODE_ENV === 'development',
+        register: true,
+        scope: '/pwa-sample',
+        sw: 'service-worker.js',
         subdomainPrefix: '/pwa-sample'
     },
     basePath: process.env.ASSETS_PREFIX,
