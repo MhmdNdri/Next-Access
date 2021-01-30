da7960cdd237c90a1e2f8667f173b5cce40c9d88
mhdmndri <m.n.mohammad.naderi@gmail.com>
2021-01-30 18:19:08 +0330

    update: service worker

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
+
+import { useEffect } from "react"
 import '../styles/globals.css'
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/pwa-sample/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
   return (
     <div>
       <Head>
