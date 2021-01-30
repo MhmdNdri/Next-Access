1ead010a527a3917df4fd07c50dab4b636daa597
mhdmndri <m.n.mohammad.naderi@gmail.com>
2021-01-30 17:11:03 +0330

    add pwa-next

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const withPWA = require('next-pwa')
 
 module.exports = withPWA({
     pwa: {
-        dest: 'out'
+        disable: process.env.NODE_ENV === 'development',
+        register: true,
+        scope: '/pwa-sample',
+        sw: 'service-worker.js',
+        //...
     },
     basePath: process.env.ASSETS_PREFIX,
     assetPrefix: process.env.ASSETS_PREFIX,
