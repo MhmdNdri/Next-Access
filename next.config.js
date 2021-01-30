3a3cdef6463c30f56b76b93c2c07074f0ee2781f
mhdmndri <m.n.mohammad.naderi@gmail.com>
2021-01-30 14:41:16 +0330

    add: env-file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 const withPWA = require('next-pwa')
 
+
 module.exports = withPWA({
     pwa: {
         dest: 'public'
-    }
+    },
+    basePath: process.env.ASSETS_PREFIX,
+    assetPrefix: process.env.ASSETS_PREFIX,
+    images: {
+        loader: 'imgix',
+        path: process.env.ASSETS_PREFIX,
+    },
 })
