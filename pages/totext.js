8475d30465fef525809d4f65d887e6028bbdd847
mhdmndri <m.n.mohammad.naderi@gmail.com>
2021-01-31 20:48:28 +0330

    fix: deploy bug

--- pages/totext.js
+++ pages/totext.js
@@ -3,29 +3,29 @@ import styles from '../styles/ToText.module.css'
 
 const SpeechToText = () => {
 
-    process.browser ? window.SpeechRecognition : null
+    // process.browser ? window.SpeechRecognition : null
 
-    const sound = document.querySelector('.sound');
-    console.log(sound)
+    // const sound = document.querySelector('.sound');
+    // console.log(sound)
 
-    const dictate = () => {
-        recognition.start();
-        recognition.onresult = (event) => {
-            const speechToText = event.results[0][0].transcript;
+    // const dictate = () => {
+    //     recognition.start();
+    //     recognition.onresult = (event) => {
+    //         const speechToText = event.results[0][0].transcript;
 
-            paragraph.textContent = speechToText;
-        }
-    }
-    const onMic = () => {
-        dictate();
-    }
+    //         paragraph.textContent = speechToText;
+    //     }
+    // }
+    // const onMic = () => {
+    //     dictate();
+    // }
     return (
         <>
             <div className={styles.container}>
                 <div className={styles.textBox} contentEditable="true"></div>
-                <i onClick={onMic} className={`${styles.fa} ${styles.faMicrophone}`}>Mic</i>
+                <i className={`${styles.fa} ${styles.faMicrophone}`}>Mic</i>
             </div>
-            <audio className={styles.sound} src="chime.mp3"></audio>
+            {/* <audio className={styles.sound} src="chime.mp3"></audio> */}
         </>
 
     )
