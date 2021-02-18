2d4558e6e4781be841f7beda6c704d83b4ceb60b
Mhmd-ndri <m.n.mohammad.naderi@gmail.com>
2021-02-18 03:37:24 +0330

    update notification test 3

---
+++ .history/pages/test_20210218033631.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/ToText.module.css";
+
+const SpeechToText = () => {
+  return (
+    <>
+      <div className={styles.container}>Hello</div>
+    </>
+  );
+};
+
+export default SpeechToText;
