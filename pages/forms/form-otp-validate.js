315f8926e2468799efa5a35f252e08e7b969fb15
Mhmd-ndri <m.n.mohammad.naderi@gmail.com>
2021-03-13 15:41:11 +0330

    fix filling input back to old version

--- pages/forms/form-otp-validate.js
+++ pages/forms/form-otp-validate.js
@@ -6,14 +6,35 @@ const validate = () => {
   const [validation, setValidationNumber] = useState(null);
   useEffect(() => {
     if ("OTPCredential" in window) {
-      const input = document.querySelector(
-        'input[autoComplete="one-time-code"]'
-      );
-      navigator.credentials
-        .get({
-          otp: { transport: ["sms"] },
-        })
-        .then((otp) => (input.value = otp.code));
+      window.addEventListener("DOMContentLoaded", (e) => {
+        const input = document.querySelector(
+          'input[autocomplete="one-time-code"]'
+        );
+        if (!input) return;
+        // Cancel the Web OTP API if the form is submitted manually.
+        const ac = new AbortController();
+        const form = input.closest("form");
+        if (form) {
+          form.addEventListener("submit", (e) => {
+            // Cancel the Web OTP API.
+            ac.abort();
+          });
+        }
+        // Invoke the Web OTP API
+        navigator.credentials
+          .get({
+            otp: { transport: ["sms"] },
+            signal: ac.signal,
+          })
+          .then((otp) => {
+            input.value = otp.code;
+            // Automatically submit the form when an OTP is obtained.
+            if (form) form.submit();
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
     }
   });
   return (
