cbbcf13a1ab19416fcf89d3e96128e924b3c5571
Mhmd-ndri <m.n.mohammad.naderi@gmail.com>
2021-03-13 14:17:37 +0330

    test react-otp-input

--- pages/forms/form-otp-validate.js
+++ pages/forms/form-otp-validate.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import styles from "../../styles/Otp.module.css";
 import axios from "axios";
 import Link from "next/link";
+import OtpInput from "react-otp-input";
 
 const validate = () => {
   const [validation, setValidationNumber] = useState(null);
   useEffect(() => {
     if ("OTPCredential" in window) {
       const input = document.querySelector(
-        'input[autocomplete="one-time-code"]'
+        'input[autoComplete="one-time-code"]'
       );
       if (input) {
         navigator.credentials
@@ -24,7 +25,7 @@ const validate = () => {
       <div className={styles.container}>
         <p className={styles.p}>Enter the number that you get from SMS</p>
         <form className={styles.form} action="/verify-otp" method="post">
-          <input
+          {/* <input
             className={styles.input}
             type="text"
             autocomplete="one-time-code"
@@ -34,6 +35,14 @@ const validate = () => {
             onChange={(e) => setValidationNumber(e.target.value)}
             pattern="\d{4}"
             required
+          /> */}
+          <OtpInput
+            value={validation}
+            onChange={(e) => setValidationNumber(e.target.value)}
+            numInputs={4}
+            separator={<span>-</span>}
+            autoComplete="one-time-code"
+            shouldAutoFocus={true}
           />
           <input className={styles.onSubmit} type="submit" value="Submit" />
         </form>
