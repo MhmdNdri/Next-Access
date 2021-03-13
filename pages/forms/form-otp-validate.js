84a1afd3f254eee965293978cf0df414cb4a8485
Mhmd-ndri <m.n.mohammad.naderi@gmail.com>
2021-03-13 15:44:45 +0330

    fix filling input back to last commit and log

--- pages/forms/form-otp-validate.js
+++ pages/forms/form-otp-validate.js
@@ -6,35 +6,17 @@ const validate = () => {
   const [validation, setValidationNumber] = useState(null);
   useEffect(() => {
     if ("OTPCredential" in window) {
-      window.addEventListener("DOMContentLoaded", (e) => {
-        const input = document.querySelector(
-          'input[autocomplete="one-time-code"]'
-        );
-        if (!input) return;
-        // Cancel the Web OTP API if the form is submitted manually.
-        const ac = new AbortController();
-        const form = input.closest("form");
-        if (form) {
-          form.addEventListener("submit", (e) => {
-            // Cancel the Web OTP API.
-            ac.abort();
-          });
-        }
-        // Invoke the Web OTP API
+      const input = document.querySelector(
+        'input[autoComplete="one-time-code"]'
+      );
+      if (input) {
+        console.log(input.value, navigator.credentials);
         navigator.credentials
           .get({
             otp: { transport: ["sms"] },
-            signal: ac.signal,
           })
-          .then((otp) => {
-            input.value = otp.code;
-            // Automatically submit the form when an OTP is obtained.
-            if (form) form.submit();
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      });
+          .then((otp) => (input.value = otp.code));
+      }
     }
   });
   return (
