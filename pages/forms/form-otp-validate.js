29272864242eb40449ce28bbe522aae83b9943e8
Mhmd-ndri <m.n.mohammad.naderi@gmail.com>
2021-03-13 15:47:42 +0330

    fix filling input: change input.value to setValidationNumber

--- pages/forms/form-otp-validate.js
+++ pages/forms/form-otp-validate.js
@@ -15,7 +15,7 @@ const validate = () => {
           .get({
             otp: { transport: ["sms"] },
           })
-          .then((otp) => (input.value = otp.code));
+          .then((otp) => setValidationNumber(otp.code));
       }
     }
   });
